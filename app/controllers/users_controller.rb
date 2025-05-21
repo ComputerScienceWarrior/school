1aced9775940959543ad96d74d6c15bc7b4d680b
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2025-05-20 21:12:24 -0700

    creates users controller and basic actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,48 @@
 class UsersController < ApplicationController
+    before_action :find_user, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @users = User.all
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            redirect_to user_path user
+        else
+            render :new
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        user = User.update(user_params)
+
+        if user.save
+            redirect_to user_path user
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @user = User.destroy
+        redirect_to root_path
+    end
+
+    private
+
+    def find_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :username, :password, :email)
+    end
 end
